dacbf4f56e9bce0edec085b3cee22988f12a38d8
mxchex <jwu1@email.neit.edu>
2023-11-30 20:32:41 -0500

    4.2

--- Week 4/Level_4.2/js/Classes/GameObject.js
+++ Week 4/Level_4.2/js/Classes/GameObject.js
@@ -1,5 +1,5 @@
 
-function GameObject(obj={})
+function GameObject(obj)
 {	
 		this.x = canvas.width/2;
 		this.y = canvas.height/2;
@@ -11,25 +11,24 @@ function GameObject(obj={})
 		this.ay = 1;
 		this.vx = 0;
 		this.vy = 0;
-		
-		
-		//------Allows us to pass object literals into the class to define its properties--------//
+
+	//whether or not the object can jump
+	this.canJump = false;
+	this.jumpHeight = -25;
+	
+	//------Allows us to pass object literals into the class to define its properties--------//
 		//------This eliminate the need to pass in the property arguments in a specific order------------//
 		if(obj!== undefined)
 		{
 			for(value in obj)
 			{
 				if(this[value]!== undefined)
-				this[value] = obj[value];
+				{
+					this[value] = obj[value];
+				}
 			}
 		}
-	
-	
-	//whether or not the object can jump
-	this.canJump = false;
-	this.jumpHeight = -25;
-	
-
+		
 	this.drawRect = function()
 	{
 		context.save();
@@ -107,14 +106,7 @@ function GameObject(obj={})
 	/*-----Sets or gets the radius value--------*/
 	this.radius = function(newRadius)
 	{
-		 if(newRadius==undefined)
-		 {
 			return this.width/2; 
-		 }
-		 else
-		 {
-			 return newRadius;
-		 }
 	}
 	
 	//Draws the collision points

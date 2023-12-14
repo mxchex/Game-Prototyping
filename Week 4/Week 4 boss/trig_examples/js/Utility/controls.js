d6e3d857e1733e3bd8bab795d3d988d53e3fd31b
mxchex <jwu1@email.neit.edu>
2023-12-14 15:02:59 -0500

    fixed boss

---
+++ Week 4/Week 4 boss/trig_examples/js/Utility/controls.js
@@ -0,0 +1,72 @@
+var w = false;
+var a = false;
+var s = false;
+var d = false;
+var q = false;
+var E = false;
+
+
+document.addEventListener("keydown", press);
+document.addEventListener("keyup", release);
+
+function press(e)
+{
+	//---This logs key codes into the browser's console.
+	console.log(e.keyCode);
+	
+	if(e.keyCode == 87)
+	{
+		w = true;
+	}
+	if(e.keyCode == 65)
+	{
+		a = true;
+	}
+	if(e.keyCode == 83)
+	{
+		s = true;
+	}
+	if(e.keyCode == 68)
+	{
+		d = true;
+	}
+	if(e.keyCode == 81)
+	{
+		q = true;
+	}
+	if(e.keyCode == 69)
+	{
+		E = true;
+	}
+}
+
+function release(e)
+{
+	//---This logs key codes into the browser's console.
+	//console.log(e.keyCode);
+	
+	if(e.keyCode == 87)
+	{
+		w = false;
+	}
+	if(e.keyCode == 65)
+	{
+		a = false;
+	}
+	if(e.keyCode == 83)
+	{
+		s = false;
+	}
+	if(e.keyCode == 68)
+	{
+		d = false;
+	}
+	if(e.keyCode == 81)
+	{
+		q = false;
+	}
+	if(e.keyCode == 69)
+	{
+		E = false;
+	}
+}
